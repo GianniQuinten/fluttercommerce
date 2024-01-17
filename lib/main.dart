e00612406efb0882b360b5ffd5fe8c37f6b8ef46
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2024-01-17 20:49:58 +0100

    added nonfunctional navbar

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,67 @@ class MyApp extends StatelessWidget {
         '/cart': (context) => CartPage(),
         '/contact': (context) => ContactPage(),
       },
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xFF89AAE6),
+            elevation: 6,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(20),
+              ),
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'ShoeCommerce',
+                  style: TextStyle(
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+                Row(
+                  children: [
+                    NavButton(label: 'Home', route: '/'),
+                    NavButton(label: 'Shoe Overview', route: '/shoeOverview'),
+                    NavButton(label: 'Cart', route: '/cart'),
+                    NavButton(label: 'Contact', route: '/contact'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          body: child,
+        );
+      },
+    );
+  }
+}
+
+class NavButton extends StatelessWidget {
+  final String label;
+  final String route;
+
+  const NavButton({required this.label, required this.route});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.pushNamed(context, route);
+      },
+      child: Text(
+        label,
+        style: TextStyle(
+          fontFamily: 'Inter',
+          fontWeight: FontWeight.w700,
+          fontSize: 16,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
