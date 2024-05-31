20a274e9dce6d055f1a318ec4c84b03d13334ba6
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2024-05-31 10:39:05 +0200

    Made it so you now see the shoeID working on the name etc now

--- lib/src/views/shoe_details_page.dart
+++ lib/src/views/shoe_details_page.dart
@@ -1,49 +1,18 @@
 import 'package:flutter/material.dart';
-import '../services/sneaks_api_service.dart';
-import '../models/shoe_model.dart';
 import '../widget/app_bar.dart';
 
 class ShoeDetailsPage extends StatelessWidget {
   final String shoeId;
 
-  ShoeDetailsPage({required this.shoeId});
+  const ShoeDetailsPage({Key? key, required this.shoeId}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final apiService = SneaksApiService();
-
+    // Use the shoeId to fetch more details about the shoe from the API, and then display those details on this page.
     return Scaffold(
       appBar: MyAppBar(title: 'JustShoes'), // Use the AppBar widget
-      body: FutureBuilder<Map<String, dynamic>>(
-        future: apiService.fetchProductDetails(shoeId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data == null) {
-            return Center(child: Text('No data available'));
-          } else {
-            final shoe = Shoe.fromJson(snapshot.data!);
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.network(shoe.imageUrl),
-                  SizedBox(height: 16),
-                  Text(
-                    shoe.name,
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  Text(shoe.brand, style: TextStyle(fontSize: 20)),
-                  SizedBox(height: 16),
-                  Text('\$${shoe.price}', style: TextStyle(fontSize: 20)),
-                ],
-              ),
-            );
-          }
-        },
+      body: Center(
+        child: Text('Shoe ID: $shoeId'), // Just an example to show the shoeId
       ),
     );
   }
