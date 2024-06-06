b6882ddaee844c904b3bc5476168fa068294523b
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2024-06-06 13:04:40 +0200

    basic layout is there still have to make the function of the button and make it a responsive ish

--- lib/src/views/home_page.dart
+++ lib/src/views/home_page.dart
@@ -1,18 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../widget/app_bar.dart';
+import '../providers/shoe_provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(title: 'JustShoes'), // Use the AppBar widget
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: MyAppBar(title: 'JustShoes'),
+      body: Padding(
+        padding: const EdgeInsets.all(50),
+        child: Row(
           children: [
-            Text(
-              'Welcome to the Shoe Store!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            Expanded(
+              flex: 1,
+              child: Consumer<ShoeProvider>(
+                builder: (context, shoeProvider, child) {
+                  if (shoeProvider.isLoading) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+
+                  if (shoeProvider.shoes.isEmpty) {
+                    return Center(child: Text('No data available'));
+                  }
+
+                  return GridView.builder(
+                    padding: const EdgeInsets.all(8.0),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      childAspectRatio: 1.5, // Height of the images
+                    ),
+                    itemCount: 4, // Only show 4 images
+                    itemBuilder: (context, index) {
+                      final shoe = shoeProvider.shoes[index];
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: Image.network(
+                            shoe.imageUrl,
+                            fit: BoxFit.contain,
+                            errorBuilder: (context, error, stackTrace) {
+                              return Icon(Icons.error);
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            SizedBox(width: 30),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(50),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Text(
+                        'Welcome to the JustShoes Store!',
+                        style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                      )
+                    ),
+                    SizedBox(height: 50),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 30),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Method to navigate to shoes overview
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Color(0xFF246EB9),
+                          // Text color & Background color
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                14), // Corner radius
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 16,
+                          ),
+                        ),
+                        child: Text(
+                          'Explore more',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ],
         ),
