81a5dc0e37d48bb020d7abe1a2f335db488a806f
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2024-06-11 12:52:49 +0200

    Made contactpage basis layout

--- lib/src/views/contact_page.dart
+++ lib/src/views/contact_page.dart
@@ -2,21 +2,82 @@ import 'package:flutter/material.dart';
 import '../widget/app_bar.dart';
 
 class ContactPage extends StatelessWidget {
-  const ContactPage({super.key});
-
   @override
   Widget build(BuildContext context) {
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    double contactFormPadding = screenWidth * 0.3; // Calculate padding based on screen width
+
     return Scaffold(
       appBar: MyAppBar(title: 'JustShoes'), // Use the AppBar widget
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to the contact page!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(contactFormPadding, 25, contactFormPadding, 25),
+          child: Card(
+            elevation: 2,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
             ),
-          ],
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Contact us here',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Subject',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      labelText: 'Information',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle form send button
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Color(0xFF246EB9),
+                      // Text color & Background color
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14), // Corner radius
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 16,
+                      ),
+                    ),
+                    child: Text(
+                      'Send',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
