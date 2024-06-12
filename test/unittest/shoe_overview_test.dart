3cee13277185ec91b93e75201173067327b4db3a
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2024-06-12 14:28:14 +0200

    Made a mockShoeProvider for the shoe test

--- test/unittest/shoe_overview_test.dart
+++ test/unittest/shoe_overview_test.dart
@@ -6,21 +6,25 @@ import 'package:fluttercommerce/src/models/shoe_model.dart';
 import 'package:fluttercommerce/src/providers/shoe_provider.dart';
 import 'package:fluttercommerce/src/views/shoe_overview_page.dart';
 
-class MockShoeProvider extends Mock implements ShoeProvider {}
+class MockShoeProvider extends Mock implements ShoeProvider {
+  // Implementing a custom behavior for the shoes property
+  @override
+  List<Shoe> get shoes => List<Shoe>.generate(8, (index) => Shoe(
+    id: 'id_$index',
+    name: 'Shoe $index',
+    brand: 'Brand $index',
+    imageUrl: 'https://example.com/shoe_$index.png',
+    price: 100,
+  ));
+
+  // Implementing a custom behavior for the isLoading property
+  @override
+  bool get isLoading => false; // Assuming isLoading should be false during testing
+}
 
 void main() {
   testWidgets('Check if there are at least 8 shoes loaded', (WidgetTester tester) async {
     final mockShoeProvider = MockShoeProvider();
-
-    final shoeList = List<Shoe>.generate(8, (index) => Shoe(
-      id: 'id_$index',
-      name: 'Shoe $index',
-      brand: 'Brand $index',
-      imageUrl: '',
-      price: 100.0,
-    ));
-
-    when(mockShoeProvider.shoes).thenReturn(shoeList);
 
     await tester.pumpWidget(
       ChangeNotifierProvider<ShoeProvider>.value(
@@ -33,12 +37,11 @@ void main() {
 
     await tester.pumpAndSettle();
 
-    // Print the number of GestureDetector widgets found to debug
     final gestureDetectorFinder = find.byType(GestureDetector);
     final gestureDetectorsCount = tester.widgetList(gestureDetectorFinder).length;
     print('Number of GestureDetector widgets found: $gestureDetectorsCount');
 
-    // Verify that there are at least 8 shoe items in the grid.
-    expect(gestureDetectorFinder, findsNWidgets(8));
+    expect(gestureDetectorFinder, findsAtLeastNWidgets(8));
   });
 }
+
