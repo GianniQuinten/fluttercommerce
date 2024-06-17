38f3dc11e7e468f1dc4b9d14d17333919e55e015
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2024-06-17 12:33:11 +0200

    added rules to proceed pipeline

--- lib/src/metrics_server.dart
+++ lib/src/metrics_server.dart
@@ -2,7 +2,7 @@
 // import 'package:shelf/shelf.dart' as shelf;
 // import 'package:shelf/shelf_io.dart' as shelf_io;
 //
-// void main.yml() {
+// void main() {
 //   final registry = PrometheusClient().registry;
 //
 //   final counter = Counter(
